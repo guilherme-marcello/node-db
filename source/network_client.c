c648cbe52e9003dcf450e7533569ad1ca9fcf8b3
guilherme-marcello <guilemosm@gmail.com>
2023-10-29 07:59:33 +0000

    Implemented network send receive

--- source/network_client.c
+++ source/network_client.c
@@ -30,10 +30,97 @@ int network_connect(struct rtable_t *rtable) {
         "network_connect",
         "Failed to connect to the table server.\n"
     )) {
-        close_and_return_failure(fd);
+        network_close(rtable);
+        return -1;
     }
 
+    rtable->sockfd = fd;
     return 0;
+}
 
+MessageT *network_send_receive(struct rtable_t *rtable, MessageT *msg) {
+    if (assert_error(
+        rtable == NULL || msg == NULL,
+        "network_send_receive",
+        ERROR_NULL_POINTER_REFERENCE
+    )) return NULL;
+
+    size_t msg_size = message_t__get_packed_size(msg);
+    short msg_size_be = htons(msg_size); // reorder bytes to be
+
+    // allocate buffer with message size
+    uint8_t* buffer = (uint8_t*)create_dynamic_memory(msg_size);
+    if (assert_error(
+        buffer == NULL,
+        "network_send_receive",
+        ERROR_MALLOC
+
+    )) {
+        network_close(rtable);
+        return NULL;
+    }
+
+    message_t__pack(msg, buffer);
+
+    // send size
+    if (assert_error(
+        write_n_to_sock(rtable->sockfd, &msg_size_be, sizeof(msg_size_be)) != sizeof(msg_size_be),
+        "network_send_receive",
+        "Failed to send size of message to server.\n"
+    )) {
+        destroy_dynamic_memory(buffer);
+        network_close(rtable);
+        return NULL;
+    }
+
+    // send msg buffer
+    if (assert_error(
+        write_n_to_sock(rtable->sockfd, (void*)buffer, msg_size) != msg_size,
+        "network_send_receive",
+        "Failed to send message buffer to server.\n"
+    )) {
+        destroy_dynamic_memory(buffer);
+        network_close(rtable);
+        return NULL;
+    }
+    destroy_dynamic_memory(buffer);
+
+    // size and buffer sent... receive response
+    msg_size_be = 0;
+    if (assert_error(
+        read_n_from_sock(rtable->sockfd, &msg_size_be, sizeof(msg_size_be)) != sizeof(msg_size_be),
+        "network_send_receive",
+        "Failed to receive server response message size.\n"
+    )) {
+        network_close(rtable);
+        return NULL;
+    }
+
+    // allocate memory to receive response message
+    msg_size = ntohs(msg_size_be);
+    buffer = (uint8_t*)create_dynamic_memory(msg_size);
+    if (assert_error(
+        buffer == NULL,
+        "network_send_receive",
+        ERROR_MALLOC
+    )) {
+        network_close(rtable);
+        return NULL;
+    };
+
+    MessageT *msg_response = message_t__unpack(NULL, msg_size, buffer);
+    destroy_dynamic_memory(buffer);
+    return msg_response;
+}
+
+int network_close(struct rtable_t *rtable) {
+    if (assert_error(
+        rtable == NULL,
+        "network_close",
+        ERROR_NULL_POINTER_REFERENCE
+    )) return -1;
+
+    close(rtable->sockfd);
+    return 0;
 }
 
