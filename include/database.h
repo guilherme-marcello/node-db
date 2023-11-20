c23a8fad4357caf635a7d3a36fbc230c72d3f524
guilherme-marcello <guilemosm@gmail.com>
2023-11-20 00:58:17 +0000

    Updated database header

--- include/database.h
+++ include/database.h
@@ -12,7 +12,7 @@ struct TableServerDatabase {
     int op_counter;
     pthread_mutex_t op_counter_mutex;
     
-    int computed_time_micros;
+    long long computed_time_micros;
     pthread_mutex_t computed_time_mutex;
 
     int active_clients;
@@ -21,5 +21,11 @@ struct TableServerDatabase {
 
 void database_init(struct TableServerDatabase* db, int n_lists);
 void database_destroy(struct TableServerDatabase* db);
+void db_decrement_active_clients(struct TableServerDatabase* db);
+void db_increment_active_clients(struct TableServerDatabase* db);
+void db_increment_op_counter(struct TableServerDatabase* db);
+void db_add_to_computed_time(struct TableServerDatabase* db, long long delta);
+
+int db_table_put(struct TableServerDatabase* db, char *key, struct data_t *value);
 
 #endif
