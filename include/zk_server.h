2ac532fba3ba8c83c46b9064053521852031d895
guilherme-marcello <guilemosm@gmail.com>
2023-12-11 16:55:07 +0000

    Added documentation for zk_server

--- include/zk_server.h
+++ include/zk_server.h
@@ -7,27 +7,55 @@
 
 #include <zookeeper/zookeeper.h>
 
+// Represents the context for updating child nodes
 struct ServerChildUpdateContext {
     struct TableServerReplicationData* replicator;
     struct TableServerDistributedDatabase* ddb;
 };
 
+// Represents the replication data for a table server
 struct TableServerReplicationData {
-    zhandle_t* zh;
-    char* server_node_path;
-    char* next_server_node_path;
-    struct ServerChildUpdateContext* child_update_context;
-    int valid;
+    zhandle_t* zh;                          // ZooKeeper handle
+    char* server_node_path;                 // Path of the server node
+    char* next_server_node_path;            // Path of the next server node
+    struct ServerChildUpdateContext* child_update_context; // Child update context
+    int valid;                              // Flag indicating validity
 };
 
+/**
+ * @brief Handles the change of the next server in replication.
+ * 
+ * @param replicator The replication data for the table server.
+ * @param ddb The distributed database associated with the table server.
+ * @param next_node The path of the next server node.
+ */
 void handle_next_server_change(struct TableServerReplicationData* replicator, struct TableServerDistributedDatabase* ddb, char* next_node);
 
-
+/**
+ * @brief Watches the children of the ZooKeeper node and triggers updates on changes.
+ * 
+ * @param wzh The handle to the ZooKeeper connection.
+ * @param type The type of event.
+ * @param state The state of the connection.
+ * @param zpath The path of the ZooKeeper node.
+ * @param watcher_ctx The context for the child watcher.
+ */
 void zk_server_child_watcher(zhandle_t* wzh, int type, int state, const char* zpath, void* watcher_ctx);
 
+/**
+ * @brief Initializes the ZooKeeper server with replication settings.
+ * 
+ * @param replicator The replication data for the table server.
+ * @param ddb The distributed database associated with the table server.
+ * @param options The options for the table server.
+ */
 void zk_server_init(struct TableServerReplicationData* replicator, struct TableServerDistributedDatabase* ddb, struct TableServerOptions* options);
 
+/**
+ * @brief Destroys the resources associated with the ZooKeeper server.
+ * 
+ * @param replicator The replication data for the table server.
+ */
 void zk_server_destroy(struct TableServerReplicationData* replicator);
-
 
 #endif
